b5fde46f2fa2c8416753b9ff0d47c8194b99e421
Cafra2 <101612462+Cafra2@users.noreply.github.com>
2023-06-08 20:42:44 -0300

    Add files via upload

--- app.s
+++ app.s
@@ -6,18 +6,13 @@ main:
     // x0 contiene la direccion base del framebuffer
 	mov x20, x0 // Guarda la dirección base del framebuffer en x20
 	//-----------------------------------------------------------------
-	movz x10, 0xFF, lsl 16
-	movk x10, 0xFFFF, lsl 00
+	movz x10, 0x76, lsl 16
+	movk x10, 0xD14D, lsl 00
 	bl pintarfondo
 
 	mov x1, 200
-	mov x2, 300
-	mov x3, 80
-	mov x4, 50
-	movz x10, 0x00, lsl 16
-	movk x10, 0x0000, lsl 00
-
-	bl cuadrado
-
+	mov x2, 200
+	bl bomber
+	
 InfLoop:
 	b InfLoop
--- graficos.s
+++ graficos.s
@@ -90,5 +90,154 @@ pintarfondo:
 ret // Pinta el fondo con un color dado
 
 //-------------------------------------------------------------------------------------------
+// Sprites
+bomber:
+    sub sp,sp, 48
+	stur x1, [sp, 0]
+    stur x2, [sp, 8]
+    stur x3, [sp, 16]
+	stur x4, [sp, 24]
+	stur lr, [sp, 32]
+    stur x10, [sp, 40]
+
+    // Cabeza
+	movz x10, 0xFF, lsl 16
+	movk x10, 0xFFFF, lsl 00 // Blanco #FFFFFF
+    add x1, x1, 24
+	add x2, x2, 16
+	mov x3, 64
+	mov x4, 72
+	bl cuadrado
+	
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 8
+	add x2, x2, 24
+	mov x3, 16
+	mov x4, 56
+	bl cuadrado
+	
+	add x1, x1, 80
+	bl cuadrado
+
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 0
+	add x2, x2, 32
+	mov x3, 8
+	mov x4, 40
+	bl cuadrado
+
+	add x1, x1, 104
+	bl cuadrado
+
+	// Antena
+	movz x10, 0xFF, lsl 16
+	movk x10, 0x1048, lsl 00 // Rojo-Rosado #FF1048
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 48
+	add x2, x2, 0
+	mov x3, 16
+	mov x4, 16
+	bl cuadrado
+
+	// Manos
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 8
+	add x2, x2, 96
+	mov x3, 16
+	mov x4, 16
+	bl cuadrado
+
+	add x1, x1, 80
+	bl cuadrado
+
+	// Patas
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 16
+	add x2, x2, 128
+	mov x3, 32
+	mov x4, 16
+	bl cuadrado
+
+	add x1, x1, 48
+	bl cuadrado
+
+	// Cara
+	movz x10, 0xFF, lsl 16
+	movk x10, 0xE0CB, lsl 00 // Piel #FFE0CB
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 16
+	add x2, x2, 32
+	mov x3, 80
+	mov x4, 40
+	bl cuadrado
+	
+	// Ojos
+	movz x10, 0 // Negro #000000
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 32
+	add x2, x2, 40
+	mov x3, 8
+	mov x4, 24
+	bl cuadrado
+
+	add x1, x1, 40
+	bl cuadrado
+
+	// Cuerpo
+	movz x10, 0x42, lsl 16
+	movk x10, 0x36ED, lsl 00 // Azul #4236ED
+	ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    add x1, x1, 32
+	add x2, x2, 88
+	mov x3, 48
+	mov x4, 40
+	bl cuadrado
+
+    ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+	add x1, x1, 24
+	add x2, x2, 96
+	mov x3, 8
+	mov x4, 24
+	bl cuadrado
+
+	add x1, x1, 56
+	bl cuadrado
+
+	// Cinturon
+	movz x10, 0 // Negro #000000
+    ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+	add x1, x1, 24
+	add x2, x2, 112
+	mov x3, 24
+	mov x4, 8
+	bl cuadrado
+
+	add x1, x1, 40
+	bl cuadrado
+
+	movz x10, 0xEE, lsl 16
+	movk x10, 0xDC00, lsl 00 // Dorado #EEDC00
+	sub x1, x1, 16
+	sub x3, x3, 8
+	bl cuadrado
+
+    ldur x1, [sp, 0]
+    ldur x2, [sp, 8]
+    ldur x3, [sp, 16]
+	ldur x4, [sp, 24]
+	ldur lr, [sp, 32]
+    ldur x10, [sp, 40]
+	add sp,sp, 40
+ret // Literalmente Bomberman!
 
 .endif
