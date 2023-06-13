139bafb3045576c0b3a68ef2938932c0d94d6c69
Cafra2 <101612462+Cafra2@users.noreply.github.com>
2023-06-13 17:33:51 -0300

    Add files via upload

---
+++ ejercicio1/app.s
@@ -0,0 +1,35 @@
+.include "graficos.s"
+.include "constantes.s"
+.include "gpiom.s"
+.globl main
+
+main:
+    // x0 contiene la direccion base del framebuffer
+	mov x20, x0 // Guarda la dirección base del framebuffer en x20
+	//-----------------------------------------------------------------
+	
+	
+	mov x1, 200
+	mov x2, 200
+	bl backdia
+	bl bomber
+
+	// Ejemplo de uso de gpios
+	mov x9, GPIO_BASE
+
+	// Atención: se utilizan registros w porque la documentación de broadcom
+	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+
+	// Setea gpios 0 - 9 como lectura
+	str wzr, [x9, GPIO_GPFSEL0]
+	ldr x12, =0x800000
+
+
+	mov x15, 0
+// w, a, s, d, espacio
+InfLoop:
+	bl cambiafondos
+
+	b InfLoop
+
+	
